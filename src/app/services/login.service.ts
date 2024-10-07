e4808fa4fbd044f9ec31205065c7e78cbc10cb58
Harshada Bhondave <harshadabhondave09@gmail.com>
2024-10-07 23:23:33 +0530

    added environments files in order to deploy the project

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
+  private apiURL = `${environment.api}/user/login`
   constructor(private http:HttpClient) { }
 
   loginUser(loginData:any){
-    return this.http.post('http://localhost:3000/user/login',loginData)
+    return this.http.post(this.apiURL,loginData)
   }
 }
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
-
+  
+   private apiURL = `${environment.api}/user/register`
   constructor(private http:HttpClient) { }
 
   adduser(body:any){
-    return this.http.post("http://localhost:3000/user/register",body)
+    return this.http.post(this.apiURL,body)
   }
 }
